5bb503f115e331d3f4619b0b8164482b14544b17
Michal Zagrodzki <michalzagrodzki@gmail.com>
2020-05-20 22:35:35 +0200

    Add outline to project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+import sceneGame from './scenes/game'
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 608,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: [
+    sceneGame
+  ]
+};
+
+export default new Phaser.Game(config);
